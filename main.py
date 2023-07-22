a39253ff7e0be6240b90d517a0c23ffe61370fc8
adamr <adam.rutkowski9033@gmail.com>
2023-07-22 02:23:32 +0200

    Slight cleanup

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-import selenium.common
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
 
-# image's url that will replace the unwanted pfp
+# Image's url that will replace the unwanted pfp
 image_url = "https://media.discordapp.net/attachments/571106990726840324/1124650634981945424/image.png"
 
 
 def change_image():
-    # Part of unwanted pfp's link
+    # Middle part of the unwanted profile picture's link
     src_value = "284037898603462666/16d540c2044b8f53ceba3317b4e7efd4"
 
     # Wait until an instant of the pfp is found
@@ -26,7 +25,8 @@ def change_image():
 
 
 if __name__ == "__main__":
-    # Get user chrome's data, so that it opens the browser and logs automatically to discord, or more easily
+    # Get Chrome's user data, to open the Chrome instance with default settings
+    # Mostly for automatically logging into discord, if logged on before
     username = os.getlogin()
     user_data_directory = f"C:/Users/{username}/AppData/Local/Google/Chrome/User Data"
     options = webdriver.ChromeOptions()
